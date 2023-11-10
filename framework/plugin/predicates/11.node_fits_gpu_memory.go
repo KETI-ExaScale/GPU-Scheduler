106f3e63f467dc4e5ef2ba5eec2a4625ff1c40ec
juhyunkil <wngus0331@gmail.com>
2023-11-10 11:02:38 +0900

    predicates 11

--- framework/plugin/predicates/11.node_fits_gpu_memory.go
+++ framework/plugin/predicates/11.node_fits_gpu_memory.go
@@ -16,18 +16,18 @@ func (pl NodeFitsGPUMemory) Debugg() {
 }
 
 func (pl NodeFitsGPUMemory) Filter(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo) {
-	for nodeName, nodeinfo := range nodeInfoCache.NodeInfoList {
-		if !nodeinfo.PluginResult.IsFiltered {
-			for gpuName, gpu := range nodeinfo.GPUMetrics {
-				if !nodeinfo.PluginResult.GPUScores[gpuName].IsFiltered {
-					if gpu.GPUMemoryFree < newPod.RequestedResource.GPUMemoryRequest {
-						reason := fmt.Sprintf("GPUMemoryFree=%d < PodRequestedGPUMemory=%d", gpu.GPUMemoryFree, newPod.RequestedResource.GPUMemoryRequest)
-						filterState := r.FilterStatus{r.UnschedulableAndUnresolvable, pl.Name(), reason, nil}
-						nodeinfo.PluginResult.GPUScores[gpuName].FilterGPU(nodeName, gpuName, filterState)
-						nodeinfo.PluginResult.GPUCountDown()
-					}
-				}
-			}
-		}
-	}
+	// for nodeName, nodeinfo := range nodeInfoCache.NodeInfoList {
+	// 	if !nodeinfo.PluginResult.IsFiltered {
+	// 		for gpuName, gpu := range nodeinfo.GPUMetrics {
+	// 			if !nodeinfo.PluginResult.GPUScores[gpuName].IsFiltered {
+	// 				if gpu.GPUMemoryFree < newPod.RequestedResource.GPUMemoryRequest {
+	// 					reason := fmt.Sprintf("GPUMemoryFree=%d < PodRequestedGPUMemory=%d", gpu.GPUMemoryFree, newPod.RequestedResource.GPUMemoryRequest)
+	// 					filterState := r.FilterStatus{r.UnschedulableAndUnresolvable, pl.Name(), reason, nil}
+	// 					nodeinfo.PluginResult.GPUScores[gpuName].FilterGPU(nodeName, gpuName, filterState)
+	// 					nodeinfo.PluginResult.GPUCountDown()
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
