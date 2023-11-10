9bbbed1c6ff6040556f6685632f224c6fe9d7c1f
juhyunkil <wngus0331@gmail.com>
2023-11-10 11:02:01 +0900

    interface.go

--- framework/interface.go
+++ framework/interface.go
@@ -1,161 +1,67 @@
 package framework
 
 import (
-	"fmt"
 	"gpu-scheduler/framework/plugin/predicates"
-	"gpu-scheduler/framework/plugin/priorities"
 	r "gpu-scheduler/resourceinfo"
 )
 
 type GPUSchedulerInterface interface {
-	//RunPreFilteringPlugins()
-	RunFilteringPlugins(*r.NodeCache, *r.QueuedPodInfo)
-	//RunPostFilteringPlugins()
-	RunScoringPlugins(*r.NodeCache, *r.QueuedPodInfo)
+	RunNodeFilteringPlugins(*r.NodeCache, *r.QueuedPodInfo)
+	RunGPUTemperatureFilteringPlugin(*r.NodeCache, *r.QueuedPodInfo)
+	RunGPUCountFilteringPlugin(*r.NodeCache, *r.QueuedPodInfo)
 }
 
-func GPUPodSpreadFramework() GPUSchedulerInterface {
-	fmt.Println("test printing framework called!!!!")
+func GPUPodFramework() GPUSchedulerInterface {
 	return &GPUSchedulerFramework{
-		Filtering: []FilterPlugin{
+		NodeFiltering: []FilterPlugin{
 			predicates.PodFitsHost{},
 			predicates.CheckNodeUnschedulable{},
 			predicates.PodFitsHostPorts{},
-			// predicates.PodToleratesNodeTaints{},
+			predicates.PodToleratesNodeTaints{},
 			predicates.PodTopologySpread_(),
 			predicates.InterPodAffinity_(),
 			predicates.MatchNodeSelector{},
 			predicates.CheckVolumeBinding{},
 			predicates.NoVolumeZoneConflict_(),
 			predicates.CheckVolumeRestriction{},
-			predicates.NodeFitsGPUMemory{},
+			predicates.PodFitsNodeResources{},
+			predicates.CheckNodeReserved{},
+		},
+		GPUTemperatureFiltering: []FilterPlugin{
+			predicates.CheckGPUTemperature{},
+		},
+		GPUCountFiltering: []FilterPlugin{
 			predicates.NodeFitsGPUCount{},
-			predicates.PodFitsNodeResources{},
-			predicates.CheckNodeReserved{},
 		},
-		Scoring: []ScorePlugin{
-			priorities.NodeAffinity{},
-			priorities.TaintToleration{},
-			priorities.SelectorSpread_(),
-			priorities.InterPodAffinity{},
-			priorities.PodTopologySpread_(),
-			priorities.ImageLocality{},
-			priorities.NodeResourcesLeastAllocated{},
-			priorities.BalancedNodeResourceAllocation{},
-			priorities.VolumeBinding{},
-			priorities.NodeMetricAnalysis{},
-			priorities.SetGPUFlopsScore{},
-			priorities.AllocatedPodCountInGPU{},
-			priorities.GPUUtilization{},
-			priorities.GPUMemoryUsageSpread{},
-			priorities.GPUMerticAnalysis{},
-			priorities.GPUTemperature{},
-			priorities.GPUPower{},
-			priorities.GPUBandwidth{},
-			// priorities.GPUDirectStoragePriority{},
-			// priorities.BalancedGPUProcessType{},
-		},
-	}
-}
-
-func GPUPodBinpackFramework() GPUSchedulerInterface {
-	return &GPUSchedulerFramework{
-		Filtering: []FilterPlugin{
-			predicates.PodFitsHost{},
-			predicates.CheckNodeUnschedulable{},
-			predicates.PodFitsHostPorts{},
-			// predicates.PodToleratesNodeTaints{},
-			predicates.PodTopologySpread_(),
-			predicates.InterPodAffinity_(),
-			predicates.MatchNodeSelector{},
-			predicates.CheckVolumeBinding{},
-			predicates.NoVolumeZoneConflict_(),
-			predicates.CheckVolumeRestriction{},
-			predicates.NodeFitsGPUMemory{},
-			predicates.NodeFitsGPUCount{},
-			predicates.PodFitsNodeResources{},
-			predicates.CheckNodeReserved{},
-		},
-		Scoring: []ScorePlugin{
-			priorities.NodeAffinity{},
-			priorities.TaintToleration{},
-			priorities.SelectorSpread{},
-			priorities.InterPodAffinity{},
-			priorities.PodTopologySpread{},
-			priorities.ImageLocality{},
-			priorities.NodeResourcesLeastAllocated{},
-			priorities.BalancedNodeResourceAllocation{},
-			priorities.VolumeBinding{},
-			priorities.NodeMetricAnalysis{},
-			priorities.SetGPUFlopsScore{},
-			priorities.AllocatedPodCountInGPU{},
-			// priorities.GPUUtilization{},
-			priorities.GPUMemoryUsageBinpack{},
-			// priorities.GPUMerticAnalysis{},
-			// priorities.GPUTemperature{},
-			// priorities.GPUPower{},
-			// priorities.GPUBandwidth{},
-			// priorities.GPUDirectStoragePriority{},
-			// priorities.BalancedGPUProcessType{},
-		},
-	}
-}
-
-func NonGPUPodFramework() GPUSchedulerInterface {
-	return &GPUSchedulerFramework{
-		Filtering: []FilterPlugin{
-			predicates.PodFitsHost{},
-			predicates.CheckNodeUnschedulable{},
-			predicates.PodFitsHostPorts{},
-			// predicates.PodToleratesNodeTaints{},
-			predicates.PodTopologySpread_(),
-			predicates.InterPodAffinity_(),
-			predicates.MatchNodeSelector{},
-			predicates.CheckVolumeBinding{},
-			predicates.NoVolumeZoneConflict_(),
-			predicates.CheckVolumeRestriction{},
-			// predicates.NodeFitsGPUMemory{},
-			// predicates.NodeFitsGPUCount{},
-			predicates.PodFitsNodeResources{},
-			predicates.CheckNodeReserved{},
-		},
-		Scoring: []ScorePlugin{
-			priorities.NodeAffinity{},
-			priorities.TaintToleration{},
-			priorities.SelectorSpread{},
-			priorities.InterPodAffinity{},
-			priorities.PodTopologySpread{},
-			priorities.ImageLocality{},
-			priorities.NodeResourcesLeastAllocated{},
-			priorities.BalancedNodeResourceAllocation{},
-			priorities.VolumeBinding{},
-			priorities.NodeMetricAnalysis{},
-		},
-	}
-}
-
-func InitNodeScoreFramework() GPUSchedulerInterface {
-	return &GPUSchedulerFramework{
-		Filtering: []FilterPlugin{}, //only scoring
-		Scoring: []ScorePlugin{
-			priorities.NodeResourcesLeastAllocated{},
-			priorities.BalancedNodeResourceAllocation{},
-			priorities.NodeMetricAnalysis{},
-			priorities.SetGPUFlopsScore{},
-			priorities.AllocatedPodCountInGPU{},
-			priorities.GPUUtilization{},
-			priorities.GPUMemoryUsageSpread{},
-			priorities.GPUMerticAnalysis{},
-			priorities.GPUTemperature{},
-			priorities.GPUPower{},
-			priorities.GPUBandwidth{},
-		},
+		// Scoring: []ScorePlugin{
+		// 	priorities.NodeAffinity{},
+		// 	priorities.TaintToleration{},
+		// 	priorities.SelectorSpread_(),
+		// 	priorities.InterPodAffinity{},
+		// 	priorities.PodTopologySpread_(),
+		// 	priorities.ImageLocality{},
+		// 	priorities.BalancedNodeResourceAllocation{},
+		// 	priorities.VolumeBinding{},
+		// 	// priorities.NodeResourcesLeastAllocated{},
+		// 	// priorities.NodeMetricAnalysis{},
+		// 	// priorities.SetGPUFlopsScore{},
+		// 	// priorities.AllocatedPodCountInGPU{},
+		// 	// priorities.GPUUtilization{},
+		// 	// priorities.GPUMemoryUsageSpread{},
+		// 	// priorities.GPUMerticAnalysis{},
+		// 	// priorities.GPUTemperature{},
+		// 	// priorities.GPUPower{},
+		// 	// priorities.GPUBandwidth{},
+		// 	// priorities.GPUDirectStoragePriority{},
+		// 	// priorities.BalancedGPUProcessType{},
+		// },
 	}
 }
 
 type GPUSchedulerFramework struct {
-	Filtering []FilterPlugin
-	Scoring   []ScorePlugin
+	NodeFiltering           []FilterPlugin
+	GPUTemperatureFiltering []FilterPlugin
+	GPUCountFiltering       []FilterPlugin
 }
 
 type PluginName interface {
@@ -168,25 +74,28 @@ type FilterPlugin interface {
 	Debugg()
 }
 
-type ScorePlugin interface {
-	PluginName
-	Score(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo)
-	Debugg(nodeInfoCache *r.NodeCache)
-}
-
-func (sf GPUSchedulerFramework) RunFilteringPlugins(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo) {
-	for _, fp := range sf.Filtering {
-		fp.Debugg()
-		fp.Filter(nodeInfoCache, newPod)
+func (sf GPUSchedulerFramework) RunNodeFilteringPlugins(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo) {
+	for _, nfp := range sf.NodeFiltering {
+		nfp.Debugg()
+		nfp.Filter(nodeInfoCache, newPod)
 		if nodeInfoCache.AvailableNodeCount == 0 {
 			return
 		}
 	}
 }
 
-func (sf GPUSchedulerFramework) RunScoringPlugins(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo) {
-	for _, sp := range sf.Scoring {
-		sp.Score(nodeInfoCache, newPod)
-		sp.Debugg(nodeInfoCache)
+func (sf GPUSchedulerFramework) RunGPUTemperatureFilteringPlugin(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo) {
+	sf.GPUTemperatureFiltering[0].Debugg()
+	sf.GPUTemperatureFiltering[0].Filter(nodeInfoCache, newPod)
+	if nodeInfoCache.AvailableNodeCount == 0 {
+		return
+	}
+}
+
+func (sf GPUSchedulerFramework) RunGPUCountFilteringPlugin(nodeInfoCache *r.NodeCache, newPod *r.QueuedPodInfo) {
+	sf.GPUCountFiltering[0].Debugg()
+	sf.GPUTemperatureFiltering[0].Filter(nodeInfoCache, newPod)
+	if nodeInfoCache.AvailableNodeCount == 0 {
+		return
 	}
 }
